c5d0dc4750f8b879b93ef825bd361974b30f0f8e
Kaviya <kaviyasugumar8@gmail.com>
2026-02-16 13:36:54 +0530

    [Kaviya] Added: UC2: Accept word inputs

--- src/LexicalTwistPuzzle.java
+++ src/LexicalTwistPuzzle.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
+
 public class LexicalTwistPuzzle {
 
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("Lexical Twist Puzzle");
+
+        System.out.print("Enter first word: ");
+        String word1 = sc.nextLine();
+
+        System.out.print("Enter second word: ");
+        String word2 = sc.nextLine();
+
+        System.out.println("You entered: " + word1 + " and " + word2);
+
+        sc.close();
     }
 }
